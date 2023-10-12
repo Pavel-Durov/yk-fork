3960665ba3129960006fd41ed45b0ddfa5d7572a
Pavel Durov <pavel@p3ld3v.dev>
2023-10-12 18:38:24 +0100

    temp

--- ykrt/src/thread_intercept.rs
+++ ykrt/src/thread_intercept.rs
@@ -1,11 +1,49 @@
+
 use std::os::raw::{c_int, c_void};
+
+use std::{os::raw::{c_void, c_int}, ffi::CString, ptr::null_mut};
+
+use libc::{dlsym, malloc, pthread_create, pthread_attr_t, pthread_t};
+
+const SHADOW_STACK_SIZE: usize = 1000000;
+
+// fn thread_entry(arg: *mut c_void) -> *mut c_void {
+//     let str = CString::new("shadowstack_0").unwrap();
+//     let result = unsafe {
+//         let addr = dlsym(null_mut(), str.as_ptr() as *const i8);
+//         println!("@@@@@@ DLSYM add {:?}", addr);
+//         let malloc_address = malloc(SHADOW_STACK_SIZE);
+//         println!("@@@@@@ malloc_address {:?}", malloc_address);
+//         *(addr as *mut *mut c_void) = malloc_address;
+//         let result: i32 = pthread_create(thread, attr, start_routine, arg);
+//         println!("@@@@@@ pthread_create result: {:?}", result);
+//         result
+//     };
+//     return result;
+// }
 
 #[no_mangle]
 pub extern "C" fn __wrap_pthread_create(
-    _: *mut c_void,
-    _: *const c_void,
-    _: extern "C" fn(*mut c_void) -> *mut c_void,
-    _: *mut c_void,
+    thread: *mut pthread_t,
+    attr: *const pthread_attr_t,
+    start_routine: extern "C" fn(*mut libc::c_void) -> *mut libc::c_void,
+    arg: *mut libc::c_void,
 ) -> c_int {
-    todo!("Allocate and set thread-local ShadowStack instance. No support for threads yet!");
+    let str = CString::new("shadowstack_0").unwrap();
+
+    let result = unsafe {
+        let addr = dlsym(null_mut(), str.as_ptr() as *const i8);
+        println!("@@@@@@ DLSYM add {:?}", addr);
+        let malloc_address = malloc(SHADOW_STACK_SIZE);
+        println!("@@@@@@ malloc_address {:?}", malloc_address);
+        *(addr as *mut *mut c_void) = malloc_address;
+        let result: i32 = pthread_create(thread, attr, start_routine, arg);
+        println!("@@@@@@ pthread_create result: {:?}", result);
+        println!("@@@@@@ addr: {:?}", addr);
+        result
+    };
+
+    println!("Running code after calling the real pthread_create...");
+
+    result
 }
