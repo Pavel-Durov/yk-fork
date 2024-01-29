54e80f246c153ae1f1ae91e0150b4c24cb7de16d
Pavel Durov <pavel@p3ld3v.dev>
2024-01-29 18:41:14 +0000

    Add pattern matching on YKB_TRACER env var.

--- ykcapi/build.rs
+++ ykcapi/build.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 pub fn main() {
     ykbuild::apply_llvm_ld_library_path();
 
@@ -5,7 +7,11 @@ pub fn main() {
     // compilation threads have finished.
     println!("cargo:rustc-cfg=yk_llvm_sync_hack");
     println!("cargo:rerun-if-env-changed=YKB_TRACER");
-    if std::env::var("YKB_TRACER") == Ok("swt".to_owned()) {
-        println!("cargo:rustc-cfg=tracer_swt");
+    match env::var("YKB_TRACER") {
+        Ok(ref tracer) if tracer == "swt" => println!("cargo:rustc-cfg=tracer_swt"),
+        Ok(ref tracer) if tracer == "hwt" => println!("cargo:rustc-cfg=tracer_hwt"),
+        Err(env::VarError::NotPresent) => println!("cargo:rustc-cfg=tracer_hwt"),
+        Ok(x) => panic!("Unknown tracer {x}"),
+        Err(_) => panic!("Invalid value for YKB_TRACER"),
     }
 }
--- ykrt/build.rs
+++ ykrt/build.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 pub fn main() {
     ykbuild::apply_llvm_ld_library_path();
     println!("cargo:rerun-if-env-changed=YKB_TRACER");
@@ -8,9 +10,11 @@ pub fn main() {
     // FIXME: This is a temporary hack because LLVM has problems if the main thread exits before
     // compilation threads have finished.
     println!("cargo:rustc-cfg=yk_llvm_sync_hack");
-    if std::env::var("YKB_TRACER") == Ok("swt".to_owned()) {
-        println!("cargo:rustc-cfg=tracer_swt");
-    } else {
-        println!("cargo:rustc-cfg=tracer_hwt");
+    match env::var("YKB_TRACER") {
+        Ok(ref tracer) if tracer == "swt" => println!("cargo:rustc-cfg=tracer_swt"),
+        Ok(ref tracer) if tracer == "hwt" => println!("cargo:rustc-cfg=tracer_hwt"),
+        Err(env::VarError::NotPresent) => println!("cargo:rustc-cfg=tracer_hwt"),
+        Ok(x) => panic!("Unknown tracer {x}"),
+        Err(_) => panic!("Invalid value for YKB_TRACER"),
     }
 }
