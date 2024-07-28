e784875f9c7e6929b5557c616d560597c05785f8
Pavel Durov <pavel@p3ld3v.dev>
2024-07-28 13:33:58 +0100

    Enable failing tests.

--- tests/c/switch_many_guards_failing.newcg.c
+++ tests/c/switch_many_guards_failing.newcg.c
@@ -1,9 +1,7 @@
-// ## FIXME: crashes under SWT
-// ignore-if: test "$YKB_TRACER" = "swt"
 // Run-time:
 //   env-var: YKD_SERIALISE_COMPILATION=1
 //   env-var: YK_LOG=4
-//   env-var: YKD_LOG_IR=-:jit-pre-opt
+//   env-var: YKD_LOG_IR=-:aot,jit-pre-opt
 //   stdout:
 //     jihgfedcbajihgfedcbajihgfedcbajihgfedcbajihgfedcbajihgfedcbajihgfedcbajihgfedcbajihgfedcbajihgfedcba
 
--- tests/c/switch_nested_guard.newcg.c
+++ tests/c/switch_nested_guard.newcg.c
@@ -1,6 +1,5 @@
-// ## FIXME: crashes under SWT
-// ignore-if: test "$YKB_TRACER" = "swt"
 // Run-time:
+//   env-var: YKD_LOG_IR=-:aot
 //   env-var: YKD_SERIALISE_COMPILATION=1
 //   stdout:
 //     gfedcblagfedcbjagfedcbmagfedcbkagfedcbiagfedcblagfedcbjagfedcbmagfedcbkagfedcbiagfedcblagfedcbjagfedcbmagfedcbkagf
