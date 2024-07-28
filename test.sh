117209373c96a98b249711cb8fedea10cd49de23
Pavel Durov <pavel@p3ld3v.dev>
2024-07-28 16:39:51 +0100

    Update test script.

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ export YKD_NEW_CODEGEN=1
 # cargo test ::switch_many_guards_failing.newcg.c -- --nocapture |& tee ./switch_many_guards_failing.newcg.c.txt
 # cargo test ::truncate.newcg.c |& tee ./truncate.newcg.c.txt
 
-GIT_COMMIT=temp # $(git rev-parse HEAD)
+GIT_COMMIT=$(git rev-parse HEAD)
 DIST="logs/${GIT_COMMIT}"
 
 mkdir -p ${DIST}
