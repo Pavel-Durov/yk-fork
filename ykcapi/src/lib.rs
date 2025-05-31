317a364f0f5914b52ffba778786641979f65bd17
Pavel Durov <pavel@p3ld3v.dev>
2025-05-31 12:42:01 +0100

    Remove comment.

--- ykcapi/src/lib.rs
+++ ykcapi/src/lib.rs
@@ -127,18 +127,6 @@ pub extern "C" fn __ykrt_control_point_real(
     // Frame address of caller.
     frameaddr: *mut c_void,
 ) {
-    // use ykrt::trace::swt::cp::debug_print_register;
-    // println!("--------------------------------------------------------------------------");
-    // println!("__ykrt_control_point_real debug");
-    // unsafe {
-    //     debug_print_register(14, 0);
-    //     debug_print_register(13, 0);
-    //     debug_print_register(15, 0);
-    //     debug_print_register(12, 0);
-    //     debug_print_register(3, 0);
-    //     debug_print_register(0, 0);
-    // }
-    // println!("--------------------------------------------------------------------------");
     let mt = unsafe { &*mt };
     let loc = unsafe { &*loc };
     if !loc.is_null() {
